d047228deb2e5edfb9c5cb4c35cd1ef1a67bb516
ijustwanttoeat90 <rh8012@naver.com>
2023-04-01 15:28:41 -0400

    late

--- src/App.js
+++ src/App.js
@@ -1,162 +1,196 @@
 import './App.css';
-import { useState } from 'react';
-// import back from './img/back.png';
-// import {getImage} from './d.js'; why not ... 
+import { useState, useEffect } from 'react';
 
-function Box({value, onCardClick}){ 
+function Box({item, src ,id, index, CheckingCard, checked}){ 
   
-    let imgList = [
-    {src: './img/1.png', id :1, alt :"card"},
-    {src: './img/2.png', id :2, alt :"card"},
-    {src: './img/3.png', id :3, alt :"card"},
-    {src: './img/4.png', id :4, alt :"card"},
-    {src: './img/5.png', id :5, alt :"card"},
-    {src: './img/6.png', id :6, alt :"card"},
-    {src: './img/1.png', id :1, alt :"card"},
-    {src: './img/2.png', id :2, alt :"card"},
-    {src: './img/3.png', id :3, alt :"card"},
-    {src: './img/4.png', id :4, alt :"card"},
-    {src: './img/5.png', id :5, alt :"card"},
-    {src: './img/6.png', id :6, alt :"card"},
-    // {src: './img/7.png', id :7, alt :"card"},
-    // {src: './img/8.png', id :8, alt :"card"},
-    // {src: './img/9.png', id :9, alt :"card"},
-    // {src: './img/10.png', id :10, alt :"card"},
-    // {src: './img/11.png', id :11, alt :"card"},
-    // {src: './img/12.png', id :12, alt :"card"}
-  ]
+    const[selected,setSelected]= useState(false);  // to set select
+    // const[a,setA] = useState();
+    // function Test(event){
+    //     console.log(a);
 
-  // {
-  //  imgList.map((index) =>{ 
-  //   if (index.id == 0) 
-  //   <img id = {index.id} src ={index.src} alt="card"/>})
-  // }
-
-  const[selected,setSelected]= useState(false);
-  const[count,setCount] = useState(0);
-  
-  if(selected){
-    return (
-    <button className="col" onClick={(onCardClick) => {setSelected(true);}} ><img src='img/1.png' className = "front card" alt="1" />{value}
-    </button>);
-  }
-
- return( <button className="col" onClick={(onCardClick) => {setSelected(true);} } ><img src='img/back.png' className = "back card" alt="1" />{value}
-</button>
-  );
-
-}
-function App( ) {
- 
-
-
-//to suffle 
-function cardShuffle(){}
-function thirtyGrid(){}
-function SisteenGrid(){}
-function twelveGrid(){}
-
-  const [IsNext, setIsNext] = useState(true);
-  const [boxes, setBoxes] = useState(Array(12).fill(null));
-    // creates an array with 12 Elemetns, and set each to null
-
-  function handleClick(i){ // creates a copy of the  arr (nextCards) with slice() arr method
-    
-    const cardList =[boxes.slice()];
-    console.log('clicked!'); 
-    
-   
-    if(boxes[i] || boxes[i]===boxes[i-1]){ // to not to flip anymore ->Card game will take it. 
-      return;
+    // }
+    function HandleClick(event, selected,checked){
+        
+        if( event.target.selected === false) {// to select
+            // console.log(event + 'should be flipp')
+            // console.log(event.target.selected
+            setSelected(true)
+            CheckingCard(event,selected,checked);
+            // setA(a);
+            // Test(a);
+        }
+        // else{
+            setTimeout(()=>{ setSelected(false);}, 1000);
+        // }
     }
-    else 
-      return ;
-  
-    const nextCards = boxes.slice(); //updates nextCards arr to add X to the first index of boxes
-    if (IsNext) {
-      nextCards[i]= false; // if is fliped-> true 
+ 
+    if (selected ===true){ // when both selected and checked card up
+        return <> 
+        <div className = "row">
+            <button className = "col down"><img className = "card "  item={item} key = {index} id = {id} src ={src}  alt= "front card"/>
+            </button>
+        </div>
+        </>
+        } 
+    if(item.checked ===true){
+        return <> 
+        <div className = "row">
+            <button className = "col up"><img className = "card "  item={item} key = {index} id = {id}   src ={src} alt= "front card"/>
+            </button>
+        </div>
+        </>
+    }
+        // not checked, not selected
+        return <> 
+        <div className = "row"> 
+            <button className = "col "><img className = "card" onClick = {(event) => HandleClick(event)} checked={checked} selected = {selected} id = {id} key = {index} src = "./img/back.png" alt="back card"/>
+            </button>
+        </div>
+        </>
+    }
+
+
+function App() {
+    // Hints/suggestions for assignment 2. 
+    // State in App component:
+    // *** remember here that hook ERROR : 훅 에러 함수안에 넣어서 **** 
+    const imgList = [ // - An array of card objects;
+        {src: './img/1.png', id :1, alt :"card",checked :false},
+        {src: './img/2.png', id :2, alt :"card",checked :false},
+        {src: './img/4.png', id :4, alt :"card",checked :false},
+        {src: './img/5.png', id :5, alt :"card",checked :false},
+        {src: './img/3.png', id :3, alt :"card",checked :false},
+        {src: './img/6.png', id :6, alt :"card",checked :false},
+        {src: './img/1.png', id :1, alt :"card",checked :false},
+        {src: './img/2.png', id :2, alt :"card",checked :false},
+        {src: './img/3.png', id :3, alt :"card",checked :false},
+        {src: './img/4.png', id :4, alt :"card",checked :false},
+        {src: './img/5.png', id :5, alt :"card",checked :false},
+        {src: './img/6.png', id :6, alt :"card",checked :false}
+        ].sort(() => Math.random() - 0.5)
+        .map(index => ({...index}))
+
+    const[count,setCount] = useState(0);
+    const[newcard,setNewcard] = useState([...imgList])
+
+    function CardShuffle() {
+        // console.log('chekc')
+        setNewcard(imgList) 
+        // console.log(shuffle)
+        NewGame()
+    }
+
+    // checking for selected cards 
+    const[first, setFirst]=useState(null);
+    const[second,setSecond]=useState(null);
+
+    // checking for whether the lenght of matched card -> 12
+    const[matched,setMatched]= useState([])
+
+    // boolean to determine if the card is matched or unmatched
+    function CheckingCard(event, selected,checked){
+        // console.log(event.target)
+        // console.log(newcard.id)
+        // console.log('cec' + event.target.checked)
+    
+        if (event.target.checked === true){ // do nothing when card up
+        }
+        else{
+
+        if (first===null ){ 
+            setCount (count+1)
+            setFirst(first + Math.floor(event.target.id)); // set first with id number 
+        }
+        else if(first !==null && second ===null){
+    
+            setSecond(second + Math.floor(event.target.id)); // set sec with id number 
+        } 
+        // else if ( first !== null && second !==null){
+        //     // console.log('>>>>')
+        //     if ( first === second){  
+        //         console.log('ddd')
+        // }}
+    }
+    }
+
+    function ReMap (){
+        // using array to update matched card 
+        const set = newcard.map(item =>{
+            if( item.id !== first){
+                return { ...item}
+            }
+            else{
+                return{ ...item, checked :true}
+            }
+            });
+            setNewcard(set) // new card map
+    }
+
+    useEffect(() => {
+    if (first !=null && second != null) {
+        if(first === second){
+        // console.log({first})
+        // console.log({second})
+        ReMap(first,second) // to set up with theh new 
+        setMatched([...matched,first,second]) // update matched card
+        // console.log({matched})
+        setFirst(null) // set null back
+        setSecond(null)
     } 
     else{
-      nextCards[i]=false; // if not -> flase 
-       
+        // console.log({first})
+        // console.log({second})
+        setFirst(null)
+        setSecond(null)
     }
-     // calling the setBoxes for react konw the state of the component has changed
-    setBoxes(nextCards);
-    setIsNext(!IsNext); 
     }
 
-   
-  const [Image,reSet] = useState([]);
-  const [count,turn] = useState(0);
+    if(matched !==null){
+        if (matched.length ===12)
+        { alert('Comletedw with '+  count + ' turns'); // when length is 12,done
+        setMatched([])}
+    }
+    },[matched,first,second,count])
 
 
-  // let imgList = [
-  //   {src: './img/1.png', id :1, alt :"card"},
-  //   {src: './img/2.png', id :2, alt :"card"},
-  //   {src: './img/3.png', id :3, alt :"card"},
-  //   {src: './img/4.png', id :4, alt :"card"},
-  //   {src: './img/5.png', id :5, alt :"card"},
-  //   {src: './img/6.png', id :6, alt :"card"},
-  //   {src: './img/1.png', id :1, alt :"card"},
-  //   {src: './img/2.png', id :2, alt :"card"},
-  //   {src: './img/3.png', id :3, alt :"card"},
-  //   {src: './img/4.png', id :4, alt :"card"},
-  //   {src: './img/5.png', id :5, alt :"card"},
-  //   {src: './img/6.png', id :6, alt :"card"},
-  //   // {src: './img/7.png', id :7, alt :"card"},
-  //   // {src: './img/8.png', id :8, alt :"card"},
-  //   // {src: './img/9.png', id :9, alt :"card"},
-  //   // {src: './img/10.png', id :10, alt :"card"},
-  //   // {src: './img/11.png', id :11, alt :"card"},
-  //   // {src: './img/12.png', id :12, alt :"card"}
-  // ]
+    function NewGame(){ // recet count and matched card
+        setCount (0)
+        setMatched([])
+    }
+        
+    function twelveGrid(){ 
+        CardShuffle()
+    }
+    function SisteenGrid(){
+        alert('NOT UPDATED')
+        
+    }
 
-  
+    function ThiryGrid(){
+        alert('NOT UPDATED')
+    }
+    
+    return (
 
-  return (
-   
     <div className="App">
-      {/* {
-   imgList.map((index) => 
-   <img id = {index.id} src ={index.src} alt="card"/>) 
-  }
-   */}
       <header className="App-header">MEMORY GAME</header>
       <div className="btnScr">
         <span className ="turn" > Turn :  {count}  </span>
         <div className="btn">
-        <button className="btnIn BtnNG" onCardClick={() => cardShuffle()}>New Game</button>
-        <button className="btnIn BtnRow3" onCardClick={() => twelveGrid()}> 3 x 4</button>
-        <button className="btnIn BtnRow4" onCardClick={() => SisteenGrid()}> 4 x 4</button>
-        <button className="btnIn BtnRow6" onCardClick={() => thirtyGrid()}> 5 x 6</button>
+        <button className="btnIn BtnNG" onClick ={CardShuffle}> New Game</button>
+        <button className="btnIn BtnRow3" onClick = {twelveGrid}> 3 x 4</button>
+        <button className="btnIn BtnRow4" onClick= {SisteenGrid}> 4 x 4</button>
+        <button className="btnIn BtnRow6" onClick= {ThiryGrid}>5 x 6</button>
         </div>
       </div>
-     
-      <div className="gridScreen">
-        <div className = "row">
-          <Box  value={boxes[0]} id ="b1" onClick={() => handleClick(0)} />   
-          <Box  value={boxes[1]} id ="b2" onCardClick={() => handleClick(1)}/>
-          <Box value={boxes[2]} id ="b3" onCardClick={() => handleClick(2)}/>
-          <Box  value={boxes[3]} id ="b4" onCardClick={() => handleClick(3)}/> 
-        </div>
-        <div className = "row">
-          <Box value={boxes[4]}  onCardClick={() => handleClick(4)}/>
-          <Box value={boxes[5]}  onCardClick={() => handleClick(5)}/>
-          <Box value={boxes[6]} onCardClick={() => handleClick(6)}/>
-          <Box value={boxes[7]} onCardClick={() => handleClick(7)}/>
-        </div>
-        <div className = "row">
-          <Box value={boxes[8]} onCardClick={() => handleClick(8)}/>
-          <Box value={boxes[9]} onCardClick={() => handleClick(9)}/>
-          <Box value={boxes[10]} onCardClick={() => handleClick(10)}/>
-          <Box value={boxes[11]} onCardClick={() => handleClick(11)}/>
-        </div>
+      <div id = "grid" className="gridScreen">
+        <>
+        { newcard.map((item,index) =>( <Box CheckingCard={CheckingCard} item = {item} src={item.src} key={index} id = {item.id} checked ={item.checked} selected = {item.selected}/>
+        ))}
+        </>  
       </div>
     </div>
   );
-
-
 }
+
 export default App;
-
-
